52ccc297bdb5dbccb10c2e1aa5a7c657a37c1769
ljh2310 <yourheui@gmail.com>
2018-12-15 18:30:15 +0900

    proxy

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    안녕
+    {{json}}
   </div>  
 </template>
 <script>
   export default{
-    
+    data(){
+      return {
+        json:''
+      }
+    },
+    mounted(){
+      this.$axios.get('http://localhost:4000').then(
+        res => console.log(res)
+      )
+    }
   }
 </script>
 <style>
